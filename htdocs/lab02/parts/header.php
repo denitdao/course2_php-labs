01e9a402ddfe151d9f068d4437e72832428c871c
Denys <dodenisa@gmail.com>
2020-04-20 19:22:42 +0300

    font changes, laravel set up

--- htdocs/lab02/parts/header.php
+++ htdocs/lab02/parts/header.php
@@ -1,6 +1,6 @@
 <div class="row header">
     <div class="logo"><a href="?page=landing"><img src="images/Logo2.svg" alt=""></a></div>
-    <div class="name"><a href="?page=landing"><h1>Комфорт Сіті Сервіс</h1></a></div>
+    <div class="name"><a href="?page=landing"><h1><span class="thin">Комфорт Сіті Сервіс</span></h1></a></div>
     <div class="menu-open" onclick="show_menu(0)"><div class="button"><img src="images/menu-open.svg" alt=""></div></div>
     <div class="menu-close" onclick="hide_menu(0)"><div class="button-pressed"><img src="images/menu-close.svg" alt=""></div></div>
     <ul class="buttons">
---
+++ htdocs/lab03/app/Some.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App;
+
+class Some {
+    public $id, $some;
+
+}
